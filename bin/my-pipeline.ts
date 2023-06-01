a587e79f6f720adef0b2a5622e8521fba2906e3f
Sreshtha Kolluru <sreshthak15@gmail.com>
2023-05-31 22:27:25 -0500

    initial commit

---
+++ bin/my-pipeline.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { MyPipelineStack } from '../lib/my-pipeline-stack';
+
+const app = new cdk.App();
+new MyPipelineStack(app, 'MyPipelineStack', {
+  env: {
+    account: '534774412465',
+    region: 'us-east-1',
+  }
+});
+
+app.synth();

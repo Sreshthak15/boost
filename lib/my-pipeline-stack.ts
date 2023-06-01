40dc00052c9ba9734983025b162273ffc168c6e2
Sreshtha Kolluru <sreshthak15@gmail.com>
2023-05-31 22:31:02 -0500

    new

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -9,7 +9,7 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('Sreshthak15/boost', 'main'),
+        input: CodePipelineSource.gitHub('Sreshthak15/boost', 'dev'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
